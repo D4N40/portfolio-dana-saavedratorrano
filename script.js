eed9625cf95496f6a1fcbe58b47e9dd7d68a29af
D4N40 <2396239@cmontmorency.qc.ca>
2025-10-27 00:53:34 -0400

    animation gsap signature, correction carroussel

---
+++ script.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", () => {
+    gsap.from('.signature', {
+        y: 100,
+        opacity: 0,
+        delay: 0.5,
+        duration: 1.5,
+        stagger: {
+            each: 0.3
+        }
+    });
+    gsap.from('.ligne', {
+        opacity: 0,
+        delay: 3,
+        duration: 1
+    });
+
+    gsap.from('.lien', {
+        opacity: 0,
+        delay: 4
+    });
+
+});
